8ed8f2a359133b659c2c5a3341735a470d51bb63
Swapnanil Dhol <swapnanildhol@gmail.com>
2022-02-20 19:10:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReleaseNotesKit
+
+A description of this package.
---
+++ Sources/ReleaseNotesKit/ReleaseNotesKit.swift
@@ -0,0 +1,6 @@
+public struct ReleaseNotesKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
